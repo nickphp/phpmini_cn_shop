18fb44f0f1e7634f3695499f7487d8e090405cd0
zhanghaonan <51153627@qq.com>
2016-05-24 13:31:03 +0800

    初始化架构

--- Application/Controller/Order/OrderController.php
+++ Application/Controller/Order/OrderController.php
@@ -18,7 +18,23 @@ class OrderController extends OrderBaseController {
      */
     public function addOrderAction()
     {
-
+            $mData = [];
+            $mData['uid']             = '100';                  //用户id
+            $mData['order_sn']        = '20160524124508998877';   //订单流水号
+            $mData['total_amount']    = '1000';         //订单总金额
+            $mData['total_num']       = '5';       //订单总数量
+            $mData['discount_amount'] = '0'; //折扣总金额
+            $mData['discount_rate']   = '0';   //订单折扣率
+            $mData['gid']             = '5';           //商品ID
+            $mData['buy_num']         = '5';       //购买数量
+            $mData['buy_price']       = '200';     //购买价格
+            $mData['market_price']    = '200';  //市场价格
+            $mData['total_price']     = '1000';   //总价
+            $mData['discount_rate']   = '0'; //折扣率
+            $mData['add_time']        = $_SERVER['REQUEST_TIME'];      //添加时间
+            $mData['update_time']     = $_SERVER['REQUEST_TIME'];   //修改时间
+            $orderId = $this->os->addOrder($mData);
+            var_dump($orderId);
     }
     
     /**
--- Application/Service/Order/OrderService.php
+++ Application/Service/Order/OrderService.php
@@ -2,8 +2,54 @@
 namespace MShop\Service\Order;
 use MShop\Service\Common\OrderBaseService;
 class OrderService  extends OrderBaseService {
-    public function test()
+    
+    /**
+     * 添加订单服务
+     * @param type $data
+     * @return type 
+     * @throws \Exception
+     */
+    public function addOrder($data)
     {
-         echo 'test';
+        try{
+            //事物支持
+            $manager = new \Phalcon\Mvc\Model\Transaction\Manager();
+            $transaction = $manager->get();
+            
+            $order = $this->getModel('Shop','Orders');//订单实例
+            $order->setTransaction($transaction); //设置事物
+            $od['uid']             = $data['uid'];                  //用户id
+            $od['order_sn']        = $data['order_sn'];             //订单流水号
+            $od['total_amount']    = $data['total_amount'];         //订单总金额
+            $od['total_num']       = $data['total_num'];       //订单总数量
+            $od['discount_amount'] = $data['discount_amount']; //折扣总金额
+            $od['discount_rate']   = $data['discount_rate'];   //订单折扣率
+            $od['add_time']        = $data['add_time'];        //添加时间
+            $od['update_time']     = $data['update_time'];     //修改时间
+            if ($order->create($od) == false) {
+                $transaction->rollback("Can't save orders");
+            }
+            
+            $orderDetails = $this->getModel('Shop','OrdersDetails');//明细实例
+            $orderDetails->setTransaction($transaction);//设置事物
+            $odd['gid']           = $data['gid'];           //商品ID
+            $odd['oid']           = $order->id;             //订单ID(关联外键)
+            $odd['uid']           = $data['uid'];           //用户ID
+            $odd['buy_num']       = $data['buy_num'];       //购买数量
+            $odd['buy_price']     = $data['buy_price'];     //购买价格
+            $odd['market_price']  = $data['market_price'];  //市场价格
+            $odd['total_price']   = $data['total_price'];   //总价
+            $odd['discount_rate'] = $data['discount_rate']; //折扣率
+            $odd['add_time']      = $data['add_time'];      //添加时间
+            $odd['update_time']   = $data['update_time'];   //修改时间
+            if ($orderDetails->create($odd) == false) {
+                $transaction->rollback("Can't save order details");
+            }
+            $transaction->commit();//提交
+            return $order->id;//返回订单ID
+            
+        } catch (Exception $e) {
+            throw new \Exception($e);
+        }
     }
 }
