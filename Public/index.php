74888c6677851c80bd9247f2d71e9d5f15e072a9
nickphp <51153627@qq.com>
2017-05-19 07:17:37 +0800

    123

--- Public/index.php
+++ Public/index.php
@@ -3,11 +3,11 @@ header('Content-type:text/html;charset=utf8');
 define("BLOG_DEBUG", true);
 define("WEB_ROOT",__DIR__);
 try {
-    //引入核心服务文件    
+    //引入核心服务文件
     include __DIR__.'/../Application/Config/Server.php';
-    
+
 } catch(\Exception $e) {
     //捕获错误信息
     echo 'INFO: ',$e->getMessage(),'<br />';
-    echo 'LINE: ',$e->getLine();
+    echo 'LINE: ',$e->getLine();//
 }
